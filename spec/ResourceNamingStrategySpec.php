f7663174fd04829539e3ad04b810e33a080e00d9
Szymon Krajewski <szykra@gmail.com>
2015-04-30 20:41:28 +0200

    Initial commit

---
+++ spec/ResourceNamingStrategySpec.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace spec\Szykra\NamingStrategy;
+
+use ICanBoogie\Inflections;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ResourceNamingStrategySpec extends ObjectBehavior
+{
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType("Szykra\NamingStrategy\ResourceNamingStrategy");
+    }
+
+    function it_return_table_name_from_simple_class_name()
+    {
+        $this->classToTableName("User")->shouldReturn("users");
+        $this->classToTableName("Category")->shouldReturn("categories");
+        $this->classToTableName("Person")->shouldReturn("people");
+    }
+
+    function it_return_table_name_from_advanced_class_name()
+    {
+        $this->classToTableName("UserActivity")->shouldReturn("user_activities");
+        $this->classToTableName("AdvancedTechnology")->shouldReturn("advanced_technologies");
+        $this->classToTableName("task_detail")->shouldReturn("task_details");
+    }
+
+    function it_return_column_name_from_property_name()
+    {
+        $this->propertyToColumnName("firstName")->shouldReturn("first_name");
+        $this->propertyToColumnName("last_name")->shouldReturn("last_name");
+        $this->propertyToColumnName("roles")->shouldReturn("roles");
+    }
+
+    function it_return_column_name_for_embedded_property()
+    {
+        $this->embeddedFieldToColumnName("address", "street")->shouldReturn("address_street");
+        $this->embeddedFieldToColumnName("address", "flatNo")->shouldReturn("address_flat_no");
+        $this->embeddedFieldToColumnName("castData", "realName")->shouldReturn("cast_data_real_name");
+        $this->embeddedFieldToColumnName("cast_data", "fake_name")->shouldReturn("cast_data_fake_name");
+    }
+
+    function it_return_reference_column_name()
+    {
+        $this->referenceColumnName()->shouldReturn("id");
+    }
+
+    function it_return_join_column_name_from_property_name()
+    {
+        $this->joinColumnName("user")->shouldReturn("user_id");
+        $this->joinColumnName("taskActivity")->shouldReturn("task_activity_id");
+    }
+
+    function it_return_join_table_name_ascending()
+    {
+        $this->joinTableName("User", "Task")->shouldReturn("task_user");
+        $this->joinTableName("Task", "TaskActivity")->shouldReturn("task_task_activity");
+        $this->joinTableName("Home", "Person")->shouldReturn("home_person");
+    }
+
+    function it_return_join_key_column_name()
+    {
+        $this->joinKeyColumnName("User")->shouldReturn("user_id");
+        $this->joinKeyColumnName("Reporter", "identity")->shouldReturn("reporter_identity");
+    }
+
+}
---
+++ src/ResourceNamingStrategy.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Szykra\NamingStrategy;
+
+use Doctrine\ORM\Mapping\NamingStrategy;
+use ICanBoogie\Inflector;
+
+class ResourceNamingStrategy implements NamingStrategy
+{
+
+    /**
+     * @var Inflector
+     */
+    private $inflector;
+
+    public function __construct($locale = "en")
+    {
+        $this->inflector = Inflector::get();
+    }
+
+    /**
+     * Returns a table name for an entity class.
+     *
+     * @param string $className The fully-qualified class name.
+     *
+     * @return string A table name.
+     */
+    function classToTableName($className)
+    {
+        $tableName = $this->inflector->pluralize($className);
+        $tableName = $this->inflector->underscore($tableName);
+
+        return $tableName;
+    }
+
+    /**
+     * Returns a column name for a property.
+     *
+     * @param string      $propertyName A property name.
+     * @param string|null $className    The fully-qualified class name.
+     *
+     * @return string A column name.
+     */
+    function propertyToColumnName($propertyName, $className = null)
+    {
+        return $this->inflector->underscore($propertyName);
+    }
+
+    /**
+     * Returns a column name for an embedded property.
+     *
+     * @param string $propertyName
+     * @param string $embeddedColumnName
+     *
+     * @return string
+     */
+    function embeddedFieldToColumnName($propertyName, $embeddedColumnName, $className = null, $embeddedClassName = null)
+    {
+        $propertyName = $this->propertyToColumnName($propertyName);
+        $embeddedColumnName = $this->propertyToColumnName($embeddedColumnName);
+
+        return $propertyName . "_" . $embeddedColumnName;
+    }
+
+    /**
+     * Returns the default reference column name.
+     *
+     * @return string A column name.
+     */
+    function referenceColumnName()
+    {
+        return 'id';
+    }
+
+    /**
+     * Returns a join column name for a property.
+     *
+     * @param string      $propertyName A property name.
+     * @param string|null $className    The fully-qualified class name.
+     *                                  This parameter is omitted from the signature due to BC
+     *
+     * @return string A join column name.
+     */
+    function joinColumnName($propertyName/*, $className = null*/)
+    {
+        return $this->propertyToColumnName($propertyName) . '_' . $this->referenceColumnName();
+    }
+
+    /**
+     * Returns a join table name.
+     *
+     * @param string      $sourceEntity The source entity.
+     * @param string      $targetEntity The target entity.
+     * @param string|null $propertyName A property name.
+     *
+     * @return string A join table name.
+     */
+    function joinTableName($sourceEntity, $targetEntity, $propertyName = null)
+    {
+        $names = [
+            $this->propertyToColumnName($sourceEntity),
+            $this->propertyToColumnName($targetEntity)
+        ];
+
+        sort($names);
+
+        return implode("_", $names);
+    }
+
+    /**
+     * Returns the foreign key column name for the given parameters.
+     *
+     * @param string      $entityName           An entity.
+     * @param string|null $referencedColumnName A property.
+     *
+     * @return string A join column name.
+     */
+    function joinKeyColumnName($entityName, $referencedColumnName = null)
+    {
+        return $this->propertyToColumnName($entityName) . "_" . ($referencedColumnName ?: $this->referenceColumnName());
+    }
+
+}
